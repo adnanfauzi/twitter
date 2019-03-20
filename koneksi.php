39ad882c44741bcf69866c6f16e13ec32ae9731a
adnanfauzi <48472211+adnanfauzi@users.noreply.github.com>
2019-03-21 01:43:52 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    
+    $kon = mysqli_connect("localhost","id7997341_tugas2","12345","id7997341_akademik");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
