fa28eced3c4fb3ac002b24e61826945b4c3c5f74
adnanfauzi <48472211+adnanfauzi@users.noreply.github.com>
2019-06-24 21:43:52 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-    
-    $kon = mysqli_connect("localhost","id7997341_tugas2","12345","id7997341_akademik");
-    if (mysqli_connect_errno()) {
-        echo "Koneksi database gagal : " .mysqli_connect_error();
-    }
-?>
